1ff4fa25fe5bfcac574d9da763a390fedeeab765
artemisia000 <misiapierobon@gmail.com>
2021-11-19 16:56:26 +0100

    getEmail

---
+++ js/main.js
@@ -0,0 +1,38 @@
+console.log('js ok');
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        email: '',
+
+    },
+    created(){
+        this.getEmail();
+
+    },
+
+    methods: {
+
+        getEmail(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+    
+            console.log(response.data);
+
+            this.email = response.data.;
+
+             })
+
+            .catch(error => {
+            
+             console.log(error);
+            
+    });
+
+
+        }
+        
+    }
+
+
+});
